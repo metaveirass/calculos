03d6a768ed79617c547555f333efa6bf32786fcf
Manuel Silva <146575541+metaveirass@users.noreply.github.com>
2023-10-16 18:02:46 +0100

    Criar a classe Conversoes e mover método

---
+++ conversoes.cs
@@ -0,0 +1,6 @@
+/// <summary>
+    /// Conversão de temperaturas.
+    /// </summary>
+    /// <param name="conversao">A conversão a efetuar.</param>
+    /// <param name="temperatura">A temperatua a converter.</param>
+    /// <returns>Retorna o resultado da conversão da temperatura.</returns>
